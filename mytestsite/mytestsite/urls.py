c184e21d6142f2ca15d6f1273079ed1147e19859
unknown <2352598374@qq.com>
2022-03-19 14:25:46 +0800

    post json to a server and receive result of request

---
+++ mytestsite/mytestsite/urls.py
@@ -0,0 +1,25 @@
+"""mytestsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import re_path
+from mytestsite import views as view1
+from django.conf.urls import include
+from myapp import views as view2
+urlpatterns = [
+    re_path('output/', view1.index_out),
+    re_path('input/', view2.index_in),
+    re_path('app/',include("myapp.urls")),
+]
